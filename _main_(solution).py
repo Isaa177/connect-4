5ba19603325888112543943ad6e84725188feff9
Isaa177 <165339756+Isaa177@users.noreply.github.com>
2024-04-09 09:25:55 -0400

    Create _main_(solution).py
    This is our solution

---
+++ _main_(solution).py
@@ -0,0 +1,14 @@
+from hello import hello
+
+with open('READ.md' 'r') as f:
+  print(f.read())
+
+while True:
+  command = input("Enter a command:")
+  if command == "hello":
+      hello()
+  elif command == "q":
+      exit()
+  else:
+     print("I did not understand this command.")
+  
