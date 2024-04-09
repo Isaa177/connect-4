ddd169e702ecc751a09c2222ff12d864d7832db2
Isaa177 <165339756+Isaa177@users.noreply.github.com>
2024-04-09 08:47:24 -0400

    Create Board.py
    This is the board we created for our game connect/4

---
+++ Board.py
@@ -0,0 +1,11 @@
+def create_new_board():
+    pass
+
+def print_board(current_board):
+    pass
+
+def drop_piuece(current_board,column,player):
+    pass
+
+def check_winner(current_board, player):
+    pass
