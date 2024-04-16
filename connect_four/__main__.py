3656e851fd6504ed6a217a43770008c1c3f8552f
Isaa177 <165339756+Isaa177@users.noreply.github.com>
2024-04-16 09:52:25 -0400

    part 3 done

--- connect_four/__main__.py
+++ connect_four/__main__.py
@@ -1,8 +1,8 @@
 from board import create_new_board
+from board import print_board
 
 with open('README.md', 'r') as f:
   print(f.read())
-
 board = create_new_board()
 while True:
   command = input("Enter a command:")
@@ -10,15 +10,9 @@ while True:
       move()
   elif command == "quit":
       exit()
+  elif command == "print board": 
+     board= create_new_board ()
+     print_board (board)
   else:
      print("I did not understand this command.")
-  
-  board = create_new_board()
-  while True: 
-     command = input ("Enter a command")
-     if command == "print_board"
-        print_board ()
-  elif command == "quit": 
-       exit ()
-  else: 
-    print ("I did not understand this command")
+      
