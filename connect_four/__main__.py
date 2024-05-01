43312051c82b2d068097c51d55292b4fb58d3a84
Isaa177 <165339756+Isaa177@users.noreply.github.com>
2024-05-01 10:27:36 -0400

    Change turns

--- connect_four/__main__.py
+++ connect_four/__main__.py
@@ -6,12 +6,18 @@ board_de_isabela = create_new_board()
 with open('README.md', 'r') as f:
   print(f.read())
 
+current_player = "X"
 while True:
   command = input("Enter a command:")
   if command == "move":
       col_de_isabela = input("which column?")
+      col_de_isabela = int(col_de_isabela)
       row_de_isabela = get_next_open_row(board_de_isabela,col_de_isabela)
-      drop_piece(board_de_isabela,row_de_isabela,col_de_isabela,"X")
+      drop_piece(board_de_isabela,row_de_isabela,col_de_isabela,current_player)
+      if current_player == "X":
+        current_player = "O"
+      elif current_player == "O":
+        current_player = "X"
   elif command == "quit":
       exit()
   elif command == "print board": 
